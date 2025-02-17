cc4c766bcfb4b2d50e1220c150c85b788d78b064
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-17 10:38:15 +0100

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,3 @@
 #!/bin/bash
-#include <stdio.h> int main(void) { puts("Programming is like building a multilingual puzzle"); return (0); }
+#include <stdio.h>
+int main(void) { puts("\"Programming is like building a multilingual puzzle"); return (0); }
