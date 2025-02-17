dd29a210c391e2e7fc4728839b0a212f03bc9d13
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-17 15:13:59 +0100

    1-last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h> // For rand and abs functions
-#include <time.h> // For srand function
+#include <stdlib.h> /* For rand and abs functions*/
+#include <time.h> /* For srand function*/
 
 int main(void)
 {
     int n;
     int last_digit;
 
-    srand(time(0)); // Initialize the random number generator
-    n = rand() - RAND_MAX / 2; // Generate a random number and assign it to n
+    srand(time(0)); /* Initialize the random number generator */
+    n = rand() - RAND_MAX / 2; /* Generate a random number and assign it to n */
 
-    last_digit = abs(n) % 10; // Get the last digit of the number using the absolute value
+    last_digit = abs(n) % 10; /* Get the last digit of the number using the absolute value */
 
-    // Print the result with appropriate message
+    /* Print the result with appropriate message */
     printf("Last digit of %d is %d and is ", n, last_digit);
 
     if (last_digit > 5)
