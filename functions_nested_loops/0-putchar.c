199068a8d776500a6ff5332dcfe3c8664854ba1d
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-18 14:34:19 +0100

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Prints all possible combinations of single-digit numbers
+* Numbers are separated by ", " and printed in ascending order.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+// Print the string "_putchar"
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n'); // Print a new line
+
+return 0; // Return 0 to indicate the program ran successfully
+}
