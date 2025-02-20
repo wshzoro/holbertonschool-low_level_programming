14572877ecb1b937b130d93b3283608235e277e8
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-20 16:06:39 +0100

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,13 +3,13 @@
 /**
  * _islower - Checks if a character is lowercase.
  * @c: The character to check.
- * 
+ *
  * Return: 1 if c is lowercase, 0 otherwise.
  */
-int _islower(int c)
+int _islower (int c)
 {
-    if (c >= 'a' && c<= 'z')
-    return (1);
-    else
-    return (0);
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
 }
