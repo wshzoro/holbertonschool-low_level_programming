42d75d413eb5988ad5a812e9b88aeb70f4e7371e
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-20 17:57:25 +0100

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  * @n: The number to check.
  *
  * Return: 1 if n is greater than zero, 0 if n is zero, 
-   -1 if n is less than zero.
+ *         -1 if n is less than zero.
  */
 int print_sign(int n)
 {
