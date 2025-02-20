77b83350ac979f448242bbbcb9a1de5cb6b29307
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-20 12:55:31 +0100

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** main - check the code.
+*
+* Return : always 0
+*/
+void print_alphabet_x10(void)
+{
+int i, letter;
+
+for (i = 0; i 10; i++)
+{
+for (letter = 'a'; letter <= 'z'; letter ++)
+_putchar ('\n');
+}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,6 +4,7 @@
 /* Function prototypes */
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 int main(void);
 
 #endif /* MAIN_H */
