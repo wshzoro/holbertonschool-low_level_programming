b3ecabd9067a20609bbf64ae5b421e91811c32f4
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-21 11:22:04 +0100

    6-abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _abs - write a function that computes the absolutes value of an integer
- * @n: the integer to computes
- * 
- * Return: The absolute value of n
- */
+* _abs - write a function that computes the absolutes value of an integer
+* @n: the integer to computes
+* 
+* Return: The absolute value of n
+*/
 int _abs(int n)
 {
-    if (n < 0)
-    return (-n);
-    else
-    return (n);
+if (n < 0)
+return (-n);
+else
+return (n);
 }
