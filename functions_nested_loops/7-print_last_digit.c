1e46093ee9528026a3945b04488b35fe61e0f139
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-21 11:42:20 +0100

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
-/** 
+/**
 * print_last_digit - prints the last digit of a number
 * @n: The number to check
 *
-* Returns : the value of the last digit
+* Return: the value of the last digit
 */
 
-int print_last_digit (int n)
+int print_last_digit(int n)
 {
 int last_digit;
 
