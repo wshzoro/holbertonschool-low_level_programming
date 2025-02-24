228479c90e13e0325dd64ff52bd96f3706411edf
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-24 11:57:51 +0100

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check if a character is uppercase
+ *@c: the character to check
+ * Return : 1 if its true or 0 if its false
+ */
+
+ int main (int c)
+{
+    if (_isupper(c))
+
+    return (1);
+    else 
+    return (0)
+
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+int _isupper(int c);
+
+#endif /* MAIN_H */
