a83cfee0c351980c233fe361e44a7598f24fffd9
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-24 14:23:52 +0100

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiplies two integers and returns the result
+* @a: first integer
+* @b: second integer
+*
+* Return: sum of a and b
+*/
+int multi(int a, int b)
+{
+return (a * b);
+}
