a64c6151fe2cc69e6a2df7a1a334594e595eafb4
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-24 14:45:00 +0100

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_number - prints the numbers, from 0 to 9
+* 
+* Returns: followed by a new line.
+*/
+
+int print_number(void)
+
+{
+char number;
+for (number='0'; number <= '9'; number ++)
+_putchar(number);
+_putchar ('\n');
+
+}
