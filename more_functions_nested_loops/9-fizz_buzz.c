0c2be20b0122158e12115048d51d07062ba7ac60
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-25 13:00:06 +0100

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 1 to 100 with FizzBuzz rules.
- *
- * Return: Always 0 (Success).
- */
+* main - Prints numbers from 1 to 100 with FizzBuzz rules.
+*
+* Return: Always 0 (Success).
+*/
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz");
-        else if (i % 3 == 0)
-            printf("Fizz");
-        else if (i % 5 == 0)
-            printf("Buzz");
-        else
-            printf("%d", i);
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
 
-        if (i != 100) /* Ajout d'un espace sauf pour le dernier élément */
-            printf(" ");
-    }
+if (i != 100) /* Ajout d'un espace sauf pour le dernier élément */
+printf(" ");
+}
 
-    printf("\n"); /* Saut de ligne à la fin */
-    return (0);
+printf("\n"); /* Saut de ligne à la fin */
+return (0);
 }
