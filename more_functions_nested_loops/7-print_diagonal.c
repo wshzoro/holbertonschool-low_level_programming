5a7f847f6db8c64f3eafd05a32bba9e9ef83e6a5
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-25 11:10:21 +0100

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * print_diagonal - draw a diagonal line 
- * @n: the number of times the character '\' should be printed
- *
- * If n is 0 or less, the function should only print a new line
- */
+* print_diagonal - draw a diagonal line 
+* @n: the number of times the character '\' should be printed
+*
+* If n is 0 or less, the function should only print a new line
+*/
 void print_diagonal(int n)
 {
-    int i, j;
+int i, j;
 
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
 
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < i; j++)
-        {
-            _putchar(' ');
-        }
-        _putchar('\\');
-        _putchar('\n');
-    }
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
 }
