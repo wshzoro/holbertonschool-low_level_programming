f6a6769cf24d74331e47a5a29487c20dd74b23d6
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-25 13:34:35 +0100

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
 /**
- * print_triangle - Prints a triangle followed by a new line.
- * @size: The size of the triangle.
- *
- * If size is 0 or less, the function prints only a new line.
- */
+* print_triangle - Prints a triangle followed by a new line.
+* @size: The size of the triangle.
+*
+* If size is 0 or less, the function prints only a new line.
+*/
 void print_triangle(int size)
 {
-    int i, j;
+int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
 
-    /* Loop for each row */
-    for (i = 1; i <= size; i++)
-    {
-        /* Print spaces: (size - i) spaces before the '#' characters */
-        for (j = 1; j <= size - i; j++)
-        {
-            _putchar(' ');
-        }
-        /* Print '#' characters: i times */
-        for (j = 1; j <= i; j++)
-        {
-            _putchar('#');
-        }
-        /* End the current line */
-        _putchar('\n');
-    }
+/* Loop for each row */
+for (i = 1; i <= size; i++)
+{
+/* Print spaces: (size - i) spaces before the '#' characters */
+for (j = 1; j <= size - i; j++)
+{
+_putchar(' ');
+}
+/* Print '#' characters: i times */
+for (j = 1; j <= i; j++)
+{
+_putchar('#');
+}
+/* End the current line */
+_putchar('\n');
+}
 }
