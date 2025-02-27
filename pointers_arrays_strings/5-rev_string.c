d2fe477fe050e369a634f0cb77f080acef9f5584
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-27 13:55:29 +0100

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string- reverses a string
+ * @s: pointer to reverse
+ * 
+ */
+void rev_string(char *s)
+{
+    int l = 0;
+    int r = strlen(s) - 1;
+    char t;
+    while (s[r] !='\0')
+    {
+        r++;
+    }
+    r--;
+    while (l < r)
+{
+    t = s[l];
+    s[l] = s[r];
+    s[r] = t;
+
+    l++;
+    r--;
+}
+}
