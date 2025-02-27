1d71897d34fb02ffa694c97dd69110667fefaef4
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-27 13:10:16 +0100

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,14 +2,14 @@
 
 /**
 * _puts - prints a string
-* @str: pointer to string 
+* @str: pointer to string
 * followed by new line
 */
 void _puts(char *str)
 {
 while (*str != '\0')
 {
-    _putchar(*str);
+_putchar(*str);
 str++;
 }
 _putchar('\n');
