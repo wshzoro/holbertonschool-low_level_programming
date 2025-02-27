2181b349b917ec4b2088971e301a5ab44b414d44
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-27 11:06:35 +0100

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* strln - returns the lenghth of a string 
+*@s: pointer to the string
+*
+* return: length of the strin
+*/
+int _strlen(char *s)
+
+{
+int len = 0;
+    
+while (s[len] != '\0')
+{
+len++;
+}
+return len;
+}
