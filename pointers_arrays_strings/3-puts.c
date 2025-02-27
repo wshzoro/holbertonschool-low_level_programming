95ddfe17871bda14eaeda6b13432417425e8991b
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-27 11:47:01 +0100

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: pointer to string 
+ * followed by new line
+ * 
+ */
+void _puts(char *str)
+{
+    while (*str !='\0')
+    {
+        putchar (*str);
+        str++;
+    }
+    return ('\n');
+}
