f5873166098dc389c6d291bb57dee606dd26406d
Julie <emmafurlan@MacBook-Pro-de-Emma.local>
2025-02-28 13:45:57 +0100

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,7 @@
  * *_strncpy - copies a string
  * @dest: the destination string where the content is copied
  * @src: the source string to copy from
- * 
+ *
  * Return: The maximum number of char
  */
 
