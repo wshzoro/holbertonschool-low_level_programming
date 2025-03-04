61365976329fb790de0f4d0c0c90440d59644ddd
Julie <emmafurlan@MBP-de-Emma.lan>
2025-03-04 14:56:58 +0100

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strchr - locates a character in a string
+* @s: string where we located
+* @c: character to located
+*
+* Return : a pointer to dest
+*/
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return (NULL);
+}
