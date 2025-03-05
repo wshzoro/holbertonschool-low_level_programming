dfbaf531928f6182d39f9b7331bb0d0f8f97ffe7
Emma <furlan.emmanuelle@neuf.fr>
2025-03-05 18:26:35 +0100

    Mise à jour de 5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -6,7 +6,7 @@
 * @haystack: string where we find
 * @needle: substrings where we find the first occurence
 * Return: to the beginning of the located substring
-* or NULL if the substring is not found.
+* or NULL if the substring is not found
 */
 char *_strstr(char *haystack, char *needle)
 {
