74165f6e76d7011656b5fbb1cdf5d5b174734c7d
Emma <furlan.emmanuelle@neuf.fr>
2025-03-05 03:03:15 +0100

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -8,20 +8,12 @@
 *
 * Return: A pointer to the resulting string dest.
 */
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, unsigned int n)
 {
-int i = 0, j = 0;
-
-while (dest[i] != '\0')
-i++;
-while (src[j] != '\0' && j < n)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
-dest[i] = src[j];
-i++;
-j++;
+dest[i] = src[i];
 }
-
-dest[i] = '\0';
-
 return (dest);
 }
