93935a5e3c039592488a10b4a6172acf86ce1262
Emma <furlan.emmanuelle@neuf.fr>
2025-03-05 18:17:49 +0100

    5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strstr - locates a substring
+* @haystack: string where we find
+* @needle: substrings where we find the first occurence
+* Return: to the beginning of the located substring
+* or NULL if the substring is not found.
+*/
+char *_strstr(char *haystack, char *needle)
+{
+char *needle = "hello, world";
+char *haystack = "world";
+char *result;
+
+result = *_strstr(*haystack, *needle);
+printf("%s\n", result);
+}
+
