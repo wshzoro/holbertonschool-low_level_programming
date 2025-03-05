78e2c6a59f0d484d357653a072920de54f0bc89c
Emma <furlan.emmanuelle@neuf.fr>
2025-03-05 21:26:28 +0100

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -13,15 +13,15 @@ char *_strstr(char *haystack, char *needle)
 int i, j;
 
 if (*needle == '\0')
-    return (haystack);
+return (haystack);
 
 for (i = 0; haystack[i]; i++)
 {
-    for (j = 0; needle[j] && haystack[i + j] == needle[j]; j++)
-        ;
+for (j = 0; needle[j] && haystack[i + j] == needle[j]; j++)
+;
 
-    if (!needle[j])
-        return (&haystack[i]);
+if (!needle[j])
+return (&haystack[i]);
 }
 
 return (NULL);
