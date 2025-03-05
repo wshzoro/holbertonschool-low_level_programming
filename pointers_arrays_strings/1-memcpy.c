f617fadf909d2fe2d0634c96348c41c00c0ee8b8
Emma <furlan.emmanuelle@neuf.fr>
2025-03-05 03:23:22 +0100

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@
 * @src: a pointer to the source buffer from where the data is to be copied
 * @n: number of bytes to be copied
 *
-* Return : a pointer to dest
+* Return: a pointer to dest
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
