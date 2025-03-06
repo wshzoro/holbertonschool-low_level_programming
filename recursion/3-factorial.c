134cb4a5d2c5ba317926d048b3750c74bd36882f
Emma <furlan.emmanuelle@neuf.fr>
2025-03-06 14:58:53 +0100

    3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -3,19 +3,15 @@
 
 /**
 * factorial - returns the factorial of a given number
-*
+*@n: the number
 * Return: f n is lower than 0,
 * the function should return -1 to indicate an error
 */
 int factorial(int n)
 {
-if (n == 0 || n == 1)
-{
-return (1);
-}
 if (n < 0)
-{
 return (-1);
-}
-return (n * factorial(-1));
+if (n == 0)
+return (1);
+return (n * factorial(n - 1));
 }
