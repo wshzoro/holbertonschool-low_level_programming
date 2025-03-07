badb3e199321f1396e98c13e8c1ca049dc7937f3
Emma <furlan.emmanuelle@neuf.fr>
2025-03-07 10:31:55 +0100

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,15 +2,25 @@
 #include <stdio.h>
 
 /**
-* _sqrt_recursion - rerturn the natural square root of a number
-* @n: square root
-*
-* Return: If n does not have a natural square root
-* the function should return -1
+* checker - checks the input number from n to the base
+* @n: number is squared and compared against base
+* @base: base number to check
+* Return: natural square root of number base
+*/
+int checker(int n, int base)
+{
+if (n * n == base)
+return (n);
+if (n * n > base)
+return (-1);
+return (checker(n + 1, base));
+}
+/**
+* _sqrt_recursion - return the natural square root of a number n.
+* @n: number to check for square roots.
+* Return: the natural square root of number n
 */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-return (-1);
-return (_sqrt_helper(n, 1));
+return (checker(1, n));
 }
