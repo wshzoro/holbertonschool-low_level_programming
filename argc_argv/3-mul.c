6e1f2b6391d2f5dc75c7d740ba7feb6b0a03986a
Emma <furlan.emmanuelle@neuf.fr>
2025-03-07 21:45:54 +0100

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,24 +2,20 @@
 #include <stdlib.h>
 
 /**
-* main - multiplie deux nombres passés en arguments
-* @argc: nombre d'arguments
-* @argv: tableau d'arguments
+* main - Multiplies two numbers.
+* @argc: Argument count.
+* @argv: Argument vector.
 *
-* Return: 0 si succès, 1 en cas d'erreur
+* Return: 0 if successful, 1 if error.
 */
 int main(int argc, char *argv[])
 {
-int num1, num2;
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-
-printf("%d\n", num1 * num2);
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
