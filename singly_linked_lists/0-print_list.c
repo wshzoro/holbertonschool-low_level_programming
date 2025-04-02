2f04f05262c8deda2f97ccdc850ecffe32f37908
Emma <furlan.emmanuelle@neuf.fr>
2025-04-03 00:16:15 +0200

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,9 +11,9 @@
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
-while (h != NULL)
+while (h)
 {
-if (h->str != NULL)
+if (h->str == NULL)
 printf("[0] (nil)\n");
 else
 printf("[%u] %s\n", h->len, h->str);
